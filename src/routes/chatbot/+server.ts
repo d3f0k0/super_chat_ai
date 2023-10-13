d3c77a9b30ea09873faeaf00f720fd385a02492c
unknown <hellomeandyou09@gmail.com>
2023-10-13 23:03:39 +0700

    fix it to get it working

--- src/routes/chatbot/+server.ts
+++ src/routes/chatbot/+server.ts
@@ -1,19 +1,27 @@
 import type { RequestHandler } from './$types';
 import { HfInference } from "@huggingface/inference";
 import { HuggingFaceStream, StreamingTextResponse } from "ai";
-import { experimental_buildLlama2Prompt } from "ai/prompts";
+import { experimental_buildOpenAssistantPrompt } from "ai/prompts";
 import { HUGGINGFACE_KEY } from "$env/static/private";
+
 
 const hf = new HfInference( HUGGINGFACE_KEY)
 
-export const GET: RequestHandler = async ({request}) => {
+export const POST: RequestHandler = async ({request}) => {
     
     const { messages } = await request.json()
 
     const response = await hf.textGenerationStream({
-        model: 'meta-llama/Llama-2-70b-chat-hf',
-        inputs: experimental_buildLlama2Prompt(messages),
-    })
+        model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+        inputs: experimental_buildOpenAssistantPrompt(messages),
+        parameters: {
+        max_new_tokens: 200,
+      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+        typical_p: 0.2,
+        repetition_penalty: 1,
+        truncate: 1000,
+        return_full_text: false,
+    },})
 
     const stream = HuggingFaceStream(response)
     return new StreamingTextResponse(stream)
